512eefb2d13f7f6338f2bac8f5fbd8788e402f50
Kubeq0 <jakub.warczyk@wp.pl>
2024-06-20 22:05:24 +0200

    IDK działa?

---
+++ cmake-build-debug/CMakeFiles/OiAK.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OiAK.dir/Counter.cpp.obj"
+  "CMakeFiles/OiAK.dir/Counter.cpp.obj.d"
+  "CMakeFiles/OiAK.dir/main.cpp.obj"
+  "CMakeFiles/OiAK.dir/main.cpp.obj.d"
+  "OiAK.exe"
+  "OiAK.exe.manifest"
+  "OiAK.pdb"
+  "libOiAK.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OiAK.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
